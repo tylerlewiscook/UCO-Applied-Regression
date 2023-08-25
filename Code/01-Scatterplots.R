5b084adae2fea171e279db895c1df031e0a7ad21
tylerlewiscook <8811690+tylerlewiscook@users.noreply.github.com>
2023-08-25 14:10:03 -0500

    Add files via upload

---
+++ Code/01-Scatterplots.R
@@ -0,0 +1,29 @@
+# 01 - Scatterplots and Regression ---------------------------------------------
+
+library(alr4)
+library(MASS)
+library(scatterplot3d)
+
+plot(salary$ysdeg, salary$salary)
+
+plot(salary$ysdeg, salary$salary, 
+     xlab = "Years since highest degree earned", 
+     ylab = "Salary (dollars per year)", 
+     main = "Here is the main title")
+
+plot(salary$ysdeg, salary$salary, 
+     xlab = "Years since highest degree earned", 
+     ylab = "Salary (dollars per year)", 
+     col = salary$rank)
+
+plot(salary$ysdeg, salary$salary, 
+     xlab = "Years since highest degree earned", 
+     ylab = "Salary (dollars per year)", 
+     col = c("cyan", "darkred", "goldenrod")[salary$rank],
+     pch = c(1, 3, 5)[salary$rank])
+
+plot(log(forbes$pres), forbes$bp)
+
+pairs(salary[,4:6])
+
+scatterplot3d(salary[,4:6])
